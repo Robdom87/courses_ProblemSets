b1bfc92170df338d9d4ea18fcba872fd8ee61fb7
Cedar Chavez <geggas00@gmail.com>
2024-02-27 09:58:25 -0800

    finish ps0

---
+++ pythonMIT/ps0/index.py
@@ -0,0 +1,12 @@
+import math
+
+#1. Asks the user to enter a number “x”
+x = int(input("Enter a number for X: "))
+#2. Asks the user to enter a number “y”
+y = int(input("Enter a number for y: "))
+#3. Prints out number “x”, raised to the power “y”.
+raised = x ** y
+print(raised)
+#4. Prints out the log (base 2) of “x”.
+logged = math.log2(x)
+print(logged)
