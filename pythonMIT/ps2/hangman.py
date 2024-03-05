46917b98bc4604495d7b9bb289cdf861fee0465e
Cedar Chavez <geggas00@gmail.com>
2024-03-04 21:51:06 -0800

    prep testing for w hints function

--- pythonMIT/ps2/hangman.py
+++ pythonMIT/ps2/hangman.py
@@ -298,8 +298,8 @@ if __name__ == "__main__":
     # To test part 2, comment out the pass line above and
     # uncomment the following two lines.
     
-    secret_word = choose_word(wordlist)
-    hangman(secret_word)
+    # secret_word = choose_word(wordlist)
+    # hangman(secret_word)
 
 ###############
     
